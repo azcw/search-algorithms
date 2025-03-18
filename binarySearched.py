0d94ecf0916d921545ef9e58d92de0ba490b32bb
azcw <schoolgithub@cg.ms>
2025-03-18 12:17:35 +0000

    removed redundant true and false statements

--- binarySearched.py
+++ binarySearched.py
@@ -10,7 +10,6 @@ lo = 0
 hi = len(data) - 1
 
 def binarySearch(data, key, lo, hi):
-    f = False
     while lo <= hi:
         mid = (lo + hi) // 2
         if data[mid] == key:
@@ -20,7 +19,6 @@ def binarySearch(data, key, lo, hi):
             lo = mid + 1
         elif data[mid] > key:
             hi = mid - 1
-    if f == False:
-        return "Not Found"
+    return "Not Found"
     
 print(binarySearch(data, key, lo, hi))
